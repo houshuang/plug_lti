5372dbdb9cfcd12194860c05ce8d9c1a896dafd7
Stian Håklev <shaklev@gmail.com>
2015-06-21 18:16:43 +0800

    This works - argh

--- lib/grade.ex
+++ lib/grade.ex
@@ -1,6 +1,7 @@
 defmodule Grade do
   import PlugLti
   @url "https://edge.edx.org/courses/course-v1:University_of_TorontoX+INQ101x+2T2015/xblock/block-v1:University_of_TorontoX+INQ101x+2T2015+type@lti+block@6aead9666027438ca67bd5fe031c8f47/handler_noauth/grade_handler"
+  # @url "http://192.168.33.10/courses/edX/DemoX/Demo_Course/xblock/i4x:;_;_edX;_DemoX;_lti;_0b08a58e26724b5e9e194c0a9d1fffec/handler_noauth/grade_handler"
   @contents String.strip(File.read!("request.txt"))
 
   def main do
@@ -21,30 +22,30 @@ defmodule Grade do
       }\"" end)
     |> Enum.join(",")
     |> IO.inspect
-    HTTPoison.request(:post, @url, @contents, [{:Accept, "application/xml"}, {:Authorization, 
+    HTTPoison.request(:post, @url , @contents, [{:Accept, "application/xml"}, {:Authorization, 
         "OAuth realm=\"\"," <> paramstr}])
     |> IO.inspect
   end
 
   def params do
 
-  # %{"oauth_consumer_key"=>"test", "oauth_nonce"=>"3RrRU3FUwTxHEo35MwGKoB7BeUIZPUcst3ufzvo", "oauth_signature_method"=>"HMAC-SHA1", "oauth_timestamp" => "1434880106", "oauth_version" =>"1.0"}
-  %{"oauth_body_hash"        => :crypto.hash(:sha, @contents) |> Base.encode64,
-    "oauth_consumer_key"     => "test",
-    "oauth_nonce"            => nonce,
-    "oauth_signature_method" => "HMAC-SHA1",
-    "oauth_timestamp"        => timestamp,
-    "oauth_version"          => "1.0"}
+%{"oauth_consumer_key"=>"test", "oauth_nonce"=>"3RrRU3FUwTxHEo35MwGKoB7BeUIZPUcst3ufzvo", "oauth_signature_method"=>"HMAC-SHA1", "oauth_timestamp" => "1434880106", "oauth_version" =>"1.0"}
+    |> Enum.map(fn {x,y} -> {x, URI.decode_www_form(y) } end )
+    |> Enum.into(%{})
+    |> Map.put("oauth_body_hash", :crypto.hash(:sha, @contents) |> Base.encode64)
+    # %{"oauth_consumer_key"     => "test",
+    #   "oauth_nonce"            => nonce,
+    #   "oauth_signature_method" => "HMAC-SHA1",
+    #   "oauth_timestamp"        => timestamp,
+    #   "oauth_version"          => "1.0",
   end
 
   def timestamp do
     {mgsec, sec, _mcs} = :os.timestamp
 
     mgsec * 1_000_000 + sec
-    |> Integer.to_string
   end
-
   def nonce do
-    :crypto.rand_bytes(16) |> Base.encode16(case: :lower)
+    :crypto.rand_bytes(32) |> Base.encode64
   end
 end
